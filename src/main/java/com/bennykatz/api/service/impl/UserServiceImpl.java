8cf2631ba229ad11eae3f825d772ff1305c5af9c
bennykatz1012 <bennykatz1012@gmail.com>
2019-08-04 22:56:32 +0300

    added security dependency

--- src/main/java/com/bennykatz/api/service/impl/UserServiceImpl.java
+++ src/main/java/com/bennykatz/api/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.bennykatz.api.service.impl;
 import com.bennykatz.api.UserRepository;
 import com.bennykatz.api.io.entity.UserEntity;
 import com.bennykatz.api.service.UserService;
+import com.bennykatz.api.shared.Utils;
 import com.bennykatz.api.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +15,15 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils utils;
+
     @Override
     public UserDto createUser(UserDto userDto) {
 
         UserEntity storedUserDetails = userRepository.findByEmail(userDto.getEmail());
 
-        if (userRepository.findByEmail(userDto.getEmail()) == null) {
+        if (userRepository.findByEmail(userDto.getEmail()) != null) {
             throw new RuntimeException("Record already exists");
         }
 
@@ -27,7 +31,7 @@ public class UserServiceImpl implements UserService {
         BeanUtils.copyProperties(userDto, userEntity);
 
         userEntity.setEncryptedPassword("test");
-        userEntity.setUserId("testUserId");
+        userEntity.setUserId(utils.generateUserId(30));
 
         UserEntity storedUserEntity = userRepository.save(userEntity);
 
--- src/main/java/com/bennykatz/api/shared/Utils.java
+++ src/main/java/com/bennykatz/api/shared/Utils.java
@@ -2,7 +2,26 @@ package com.bennykatz.api.shared;
 
 import org.springframework.stereotype.Component;
 
+import java.security.SecureRandom;
+import java.util.Random;
+
 @Component
 public class Utils {
 
+    private final Random RANDOM = new SecureRandom();
+    private final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    public String generateUserId(int length) {
+        return generateRandomString(length);
+    }
+
+    private String generateRandomString(int length) {
+        StringBuilder stringBuilder = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            stringBuilder.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
+        }
+
+        return new String(stringBuilder);
+    }
 }
